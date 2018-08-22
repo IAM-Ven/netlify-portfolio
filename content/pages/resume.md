ebf643a5126ce5d1c8d7c7cd304581fe61c27ea5
Philip Tietjen <ptietjen@Philips-MacBook-Pro.local>
2018-08-21 22:08:30 -0400

    initialize resume layout

--- content/pages/resume.md
+++ content/pages/resume.md
@@ -1,15 +1,6 @@
 ---
 title: Resume
-subTitle: A more formal look
 intro: |-
-  ## Lorem Ipsum
+  ## A more formal look
 
-  Netlify CMS works with both `.md` and `.json`.
-
-  ![Image test](/images/uploads/isabella juskova.jpg)
-
-  [Link test](/images/uploads/isabella juskova.jpg)
-
-  ### Embed Test
-
-  <iframe width="560" height="315" src="https://www.youtube.com/embed/Js00yn142ic" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+  A hard working and enthusiastic Web Developer, excited about working with certified cool technologies like React, Node, and Express. I mostly work in E-Commerce, but am always looking to reach for new pastures or improve where I'm currently at. 
--- src/views/Resume.js
+++ src/views/Resume.js
@@ -1,22 +1,31 @@
 import React from 'react'
 
+import {Container, Row, Col} from 'reactstrap' 
 import PageHeader from '../components/PageHeader'
-import Content from '../components/Content'
 import './Resume.css'
 
 export default ({ fields }) => {
-  const { title, subtitle, intro } = fields
+  const { title, intro } = fields
   return (
     <div className='Resume'>
       <PageHeader
         title={title}
-        subtitle={subtitle}
+        subtitle={intro}
       />
-      <div className='section'>
-        <div className='container'>
-          <Content source={intro} />
-        </div>
-      </div>
+      <Container fluid>
+        <Row>
+          <Col className="p-0" lg="9" md="8" sm="12">
+            <div className="p-5">
+              <h2 className="blockTitle">Experience</h2>
+            </div>
+          </Col>
+          <Col className="p-0" lg="3" md="4" sm="12">
+          <div className="p-5">
+              <h2 className="blockTitle">Skills</h2>
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </div>
   )
 }
