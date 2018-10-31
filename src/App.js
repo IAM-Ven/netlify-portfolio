4fd7d5281a5ee0ca11db2971c317d4d021af4245
Philip Tietjen <tietjen.philip@gmail.com>
2018-10-30 23:24:18 -0400

    remove moment ref

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component, Fragment } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Helmet from 'react-helmet'
-import moment from 'moment';
 
 import ScrollToTop from './components/ScrollToTop'
 import Meta from './components/Meta'
